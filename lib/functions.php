59d75659d7398b4c837cc47f88dc984279e3187d
hk453 <90267388+hk453@users.noreply.github.com>
2021-12-11 18:43:07 -0500

    MS2 Updated functions.php in dev

--- lib/functions.php
+++ lib/functions.php
@@ -36,20 +36,9 @@ function is_valid_email($email = "")
     return filter_var(trim($email), FILTER_VALIDATE_EMAIL);
 }
 //TODO 3: User Helpers
-
 function is_logged_in()
 {
     return isset($_SESSION["user"]); //se($_SESSION, "user", false, false);
-=======
-function is_logged_in($redirect = false, $destination = "login.php")
-{
-    $isLoggedIn = isset($_SESSION["user"]);
-    if ($redirect && !$isLoggedIn) {
-        flash("You must be logged in to view this page", "warning");
-        die(header("Location: $destination"));
-    }
-    return $isLoggedIn; //se($_SESSION, "user", false, false);
-
 }
 function has_role($role)
 {
@@ -230,12 +219,6 @@ function get_top_10($duration = "day")
     return $results;
 }
 
-
-
-
-}
-
-}
 
 
 
