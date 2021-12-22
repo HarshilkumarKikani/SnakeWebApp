1a60f4c76c0e6ebe052f8860da0cf831252de937
Harshilkumar Kikani <hk453@njit.edu>
2021-12-22 00:31:20 -0500

    changes

--- partials/nav.php
+++ partials/nav.php
@@ -44,7 +44,8 @@ require_once(__DIR__ . "/../lib/functions.php");
             <li><a href="<?php echo get_url('admin/create_role.php'); ?>">Create Role</a></li>
             <li><a href="<?php echo get_url('admin/list_roles.php'); ?>">List Roles</a></li>
             <li><a href="<?php echo get_url('admin/assign_roles.php'); ?>">Assign Roles</a></li>
-            <li><a href="<?php echo get_url('admin/calc_winners.php'); ?>">Calc Roles</a></li>        <?php endif; ?>
+            <li><a href="<?php echo get_url('admin/calc_winners.php'); ?>">Assign Roles</a></li>
+        <?php endif; ?>
         <?php if (is_logged_in()) : ?>
             <li><a href="<?php echo get_url('logout.php'); ?>">Logout</a></li>
         <?php endif; ?>
